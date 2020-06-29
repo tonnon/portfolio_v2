7fb2f00080e0fa79aed0afe9143b9240fca25a84
tonnon <lucastonnon@gmail.com>
2020-06-28 22:40:17 -0300

    Build

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-const express = require('express')
-const path = require('path')
-const app = express()
+const express = require('express');
+const port = process.env.PORT || 8080;
+const app = express();
 
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')))
-  // Handle React routing, return all requests to React app
-  app.get('*', (request, response) => {
-    response.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-  })
-}
-const port = process.env.PORT || 8080
-app.listen(port, () => {
-  console.log(`API listening on port ${port}...`)
-})
+app.use(express.static(__dirname + "/build/"));
+app.get(/.*/, function(req, res){
+    res.sendFile(__dirname + "/build/index.html");
+});
+app.listen(port);
+
+console.log('Server Started');
